bcf26520c3b0bb302643f4444b173807dea7c7b4
Prathamesh_Rege <68123254+prime-netizen@users.noreply.github.com>
2022-08-10 13:15:46 -0400

    Update nin_norelu.py

--- CIFAR_10/models/nin_norelu.py
+++ CIFAR_10/models/nin_norelu.py
@@ -19,6 +19,9 @@ class BinActive(torch.autograd.Function):
         """
         bin_input=input.detach().cpu().numpy()
         file=open('Binarized Inputs.txt','a')
+        file.write('The Binarized Inputs for the layer are:')
+        file.write('/n')
+        file.write('/n')           
         for i in bin_input:
             for j in i:
                 np.savetxt(file,j)
