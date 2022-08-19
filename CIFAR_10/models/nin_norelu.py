a969dfb53bd7a46b4f122189174dc26c37d872dc
Prathamesh_Rege <68123254+prime-netizen@users.noreply.github.com>
2022-08-19 08:12:54 -0400

    Update nin_norelu.py

--- CIFAR_10/models/nin_norelu.py
+++ CIFAR_10/models/nin_norelu.py
@@ -1,4 +1,5 @@
 #Architecture NIN. Modifications: Relu,bias units removed from bin conv layers
+#Maxpooling inside BinConv2d layer 
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
