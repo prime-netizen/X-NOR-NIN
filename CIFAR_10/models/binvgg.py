3ebae35e8df787ac0ef921f7a4daaa6a8482d7cb
Prathamesh_Rege <68123254+prime-netizen@users.noreply.github.com>
2022-09-30 00:40:07 -0400

    Update binvgg.py

--- CIFAR_10/models/binvgg.py
+++ CIFAR_10/models/binvgg.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+
 class BinActive(torch.autograd.Function):
     '''
     Binarize the input activations and calculate the mean across channel dimension.
@@ -37,78 +38,11 @@ class BinActive(torch.autograd.Function):
         grad_input[input.le(-1)] = 0
         return grad_input
 
-class BinConv2d(nn.Module):
+
+    class BinConv2d(nn.Module):
     def __init__(self, input_channels, output_channels,
             kernel_size=-1, stride=-1, padding=-1, dropout=0, save_info=0):
         super(BinConv2d, self).__init__()
-        self.layer_type = 'BinConv2d'
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.dropout_ratio = dropout
-        self.save_info= save_info
-
-        self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
-        self.bn.weight.data = self.bn.weight.data.zero_().add(1.0)
-        if dropout!=0:
-            self.dropout = nn.Dropout(dropout)
-        self.conv = nn.Conv2d(input_channels, output_channels,
-                kernel_size=kernel_size, stride=stride, padding=padding)
-        #self.relu = nn.ReLU(inplace=True)
-    
-    def forward(self, x):
-        x_value = x.clone()
-        x = self.bn(x)
-        x, mean = BinActive.apply(x)
-        if self.dropout_ratio!=0:
-            x = self.dropout(x)
-        x = self.conv(x)
-        #x = BinOp.binarization(x)
-        if self.save_info:
-            save_variable(x_value,self.bn.weight.data,self.bn.bias.data,self.conv.weight.data,self.conv.bias.data, x )
-        #x = self.relu(x)
-        return x
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.xnor = nn.Sequential(
-                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-                nn.ReLU(inplace=True),
-            
-                BinConv2d(192, 160, kernel_size=1, stride=1, padding=0),
-                BinConv2d(160,  96, kernel_size=3, stride=2, padding=1),
-                #nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-
-                BinConv2d( 96, 192, kernel_size=5, stride=1, padding=2, dropout=0.5),
-                BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-                BinConv2d(192, 192, kernel_size=3, stride=2, padding=1),
-                #nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
-
-                BinConv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
-                BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            
-                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-            
-                nn.Conv2d(192,  10, kernel_size=1, stride=1, padding=0),
-                nn.ReLU(inplace=True),
-                nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
-                )
-
-    def forward(self, x):
-        for m in self.modules():
-            if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
-                if hasattr(m.weight, 'data'):
-                    m.weight.data.clamp_(min=0.01)
-        x = self.xnor(x)
-        x = x.view(x.size(0), 10)
-        return x
-    
-class Bin_Conv2d(nn.Module):
-    def __init__(self, input_channels, output_channels,
-            kernel_size=-1, stride=-1, padding=-1, dropout=0, save_info=0):
-        super(Bin_Conv2d, self).__init__()
         self.layer_type = 'Bin_Conv2d'
         self.kernel_size = kernel_size
         self.stride = stride
@@ -159,45 +93,96 @@ class Bin_Conv2d(nn.Module):
             save_variable(x_value,self.bn.weight.data,self.bn.bias.data,self.conv.weight.data,self.conv.bias.data, x )
         #x = self.relu(x)
         return x
+
     
     
-class Net_BN(nn.Module):
-    def __init__(self):
-        super(Net_BN, self).__init__()
-        self.xnor = nn.Sequential(
-                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-                nn.ReLU(inplace=True),
-            
-                Bin_Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
-                #Bin_Conv2d(160,  96, kernel_size=1, stride=1, padding=0),
-                Bin_Conv2d(160,  96, kernel_size=3, stride=2, padding=1),
-                #nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
 
-                Bin_Conv2d( 96, 192, kernel_size=5, stride=1, padding=2, dropout=0.5),
-                Bin_Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-                #Bin_Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-                Bin_Conv2d(192, 192, kernel_size=3, stride=2, padding=1),
-                #nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+cfg = {
+    'VGG11': ['M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
 
-                Bin_Conv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
-                Bin_Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-           
-                nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-            
-                nn.Conv2d(192,  10, kernel_size=1, stride=1, padding=0),
-                nn.ReLU(inplace=True),
-                nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
-                )
+
+
+class Bin_VGG_train(nn.Module):
+    def __init__(self, vgg_name):
+        super(Bin_VGG_train, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2./n))
+                m.bias.data.zero_()
 
     def forward(self, x):
-        for m in self.modules():
-            if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
-                if hasattr(m.weight, 'data'):
-                    m.weight.data.clamp_(min=0.01)
-        x = self.xnor(x)
-        x = x.view(x.size(0), 10)
-        return x
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = OrderedDict([
+            ('conv0', nn.Conv2d(3, 64, kernel_size=3, padding=1)),
+            ('bn0', nn.BatchNorm2d(64)),
+            ('relu0', nn.ReLU(inplace=True))
+            ])
+        in_channels = 64
+        cnt = 1
+        for x in cfg:
+            if x == 'M':
+                layers['pool'+str(cnt)] = nn.MaxPool2d(kernel_size=2, stride=2)
+                cnt += 1
+            else:
+                layers['conv'+str(cnt)] = BinConv2d(in_channels=in_channels, out_channels=x, kernel_size=3, padding=1)
+                cnt += 1
+                layers['bn'+str(cnt)] = nn.BatchNorm2d(x)
+                cnt += 1
+                layers['relu'+str(cnt)] = nn.ReLU(inplace=True)
+                cnt += 1
+                in_channels = x
+        layers['pool'+str(cnt)] = nn.AvgPool2d(kernel_size=1, stride=1)
+        return nn.Sequential(layers)
+
+
+class Bin_VGG_test(nn.Module):
+    def __init__(self, vgg_name):
+        super(Bin_VGG_test, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = OrderedDict([
+            ('conv0', nn.Conv2d(3, 64, kernel_size=3, padding=1)),
+            ('bn0', nn.BatchNorm2d(64)),
+            ('relu0', nn.ReLU(inplace=True))
+            ])
+        in_channels = 64
+        cnt = 1
+        for x in cfg:
+            if x == 'M':
+                layers['pool'+str(cnt)] = nn.MaxPool2d(kernel_size=2, stride=2)
+                cnt += 1
+            else:
+                layers['conv'+str(cnt)] = BinConv2d(in_channels=in_channels, out_channels=x, kernel_size=3, padding=1)
+                cnt += 1
+                layers['bn'+str(cnt)] = nn.BatchNorm2d(x)
+                cnt += 1
+                layers['relu'+str(cnt)] = nn.ReLU(inplace=True)
+                cnt += 1
+                in_channels = x
+        layers['pool'+str(cnt)] = nn.AvgPool2d(kernel_size=1, stride=1)
+        return nn.Sequential(layers)
+
+
 
     
 def save_variable(x,bn_weights,bn_bias,conv_weights,conv_bias,output):
